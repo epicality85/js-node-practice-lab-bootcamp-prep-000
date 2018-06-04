c802a7771cfa1c031e61f6b3c8abb8437d4e866b
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-04 15:29:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 
-var name = "Joe"
+var name = "Danny:"
 var height = 74
 
 console.log("Name:")
@@ -8,3 +8,6 @@ console.log(name)
 
 console.log("Height:")
 console.log(height)
+
+console.log("With some swanky heels on:")
+console.log(height + 1)
